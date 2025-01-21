534f23b5de500f2fa68f18662ba0dcbd0344b0bd
drkup <darrenkwondev46@gmail.com>
2025-01-22 00:14:33 +0900

    enum

---
+++ a008_enum.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+enum Size {Short, Tall, Grande, Venti};
+char sizeName[][7] = {"Short", "Tall", "Grande", "Venti"}; // array of [7]
+int priceAmericano[] = {3800, 4100, 4600, 5100};
+int priceCappuccino[] = {4600, 5900, 6400, 6900};
+
+int main()
+{
+  printf("커피 가격(아메리카노)\n");
+  for(int i = Short; i <= Venti; i++)
+    printf("%10s : %5d\n", sizeName[i], priceAmericano[i]);
+
+
+  printf("커피 가격(카푸치노)\n");
+  for(int i = Short; i <= Venti; i++)
+    printf("%10s : %5d\n", sizeName[i], priceCappuccino[i]);
+
+}
